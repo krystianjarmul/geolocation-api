95f4e87eeb3acaa98271feba8b6d405390bcd2cc
krystianjarmul <krystianjarmul.96@gmail.com>
2021-10-05 14:53:17 +0200

    Register models in admin site

--- geolocation/admin.py
+++ geolocation/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Language,
+    Currency,
+    Connection,
+    TimeZone,
+    Location,
+    Geolocation
+)
+
+admin.site.register(Language)
+admin.site.register(Currency)
+admin.site.register(Connection)
+admin.site.register(TimeZone)
+admin.site.register(Location)
+admin.site.register(Geolocation)
