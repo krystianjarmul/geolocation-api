075e6d4203e69f9ed3bc4b7cfc98d6a9eedefaf8
Krystian <47600159+krystianjarmul@users.noreply.github.com>
2021-10-11 17:20:56 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView
 )
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("geolocation.urls")),
